90d9cdc636fab750dc96ae2a2b29dff6ded09e6a
rayimqulovjorabek02 <rayimqulovjorabek02@gmail.com>
2026-03-30 23:05:37 +0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/env bash
+# Xatolik bo'lsa to'xtatish
+set -o errexit
 
+# Kutubxonalarni majburiy o'rnatish
+pip install --upgrade pip
+pip install python-telegram-bot gTTS SpeechRecognition pydub
+
+# Agar kodingizda 'ffmpeg' kerak bo'lsa (ovoz uchun):
+# apt-get update && apt-get install -y ffmpeg
