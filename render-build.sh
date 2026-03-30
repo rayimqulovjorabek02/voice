88d4364e3aec4cc7b2c14ebf815507d1a21c0c06
rayimqulovjorabek02 <rayimqulovjorabek02@gmail.com>
2026-03-31 03:42:14 +0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
-# Xatolik bo'lsa to'xtatish
 set -o errexit
 
 # Kutubxonalarni o'rnatish
 pip install -r requirements.txt
 
-# FFmpeg-ni majburiy o'rnatish (Render uchun)
-mkdir -p ffmpeg
-curl -L https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz | tar -xJ --strip-components=1 -C ffmpeg
-export PATH=$PATH:$(pwd)/ffmpeg/bin
+# Agar ffmpeg papkasi bo'lmasa, yaratish
+mkdir -p ffmpeg_bin
+
+# FFmpeg-ni yuklab olish (statik havola)
+# Agar birinchisi xato bersa, muqobil havola ishlatiladi
+curl -L https://github.com/eugeneware/ffmpeg-static/releases/download/b5.0.1/linux-x64 -o ffmpeg_bin/ffmpeg
+
+# Unga ruxsat berish
+chmod +x ffmpeg_bin/ffmpeg
+
+# PATH-ni tekshirish uchun (Build vaqtida)
+export PATH=$PATH:$(pwd)/ffmpeg_bin
